692062b058ed380e28a7e0100acbb03cb84b2e85
Qi021212 <3377803605@qq.com>
2025-06-27 10:21:00 +0800

    Initial

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+//导入组件
+import LayoutVue from '@/views/Menu.vue'
+import MainVue from '@/views/Main.vue'
+
+//定义路由
+const routes = [
+       { path: '/signup', component: MerchantSignupVue },
+       { path: '/login', component: MerchantLoginVue },
+    {
+        path: '/', component: LayoutVue, redirect: '/main', children: [
+            { path: '/main', component: MainVue },
+        ]
+    }
+]
+
+//创建路由
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes
+})
+
+//导出路由
+export default router
